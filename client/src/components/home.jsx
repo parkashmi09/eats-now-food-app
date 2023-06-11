aade56a089de1b547e87c3b57218d5e9bfd204af
parkash kumar <parkashmi66@gmail.com>
2023-06-11 15:21:16 +0530

    fixed home container ui

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -6,73 +6,64 @@ import { steggerFadeInOut } from "../animations";
 
 export default function Home() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full">
-      <div className="py-2 flex-1 flex flex-col items-start  justify-center gap-6">
-        <div className="flex items-center gap-2 justify-center bg-orange-100  rounded-full px-4 py-1">
-          <p className="text-base text-orange-500 font-semibold">
-            Bike Delievery
+    <section className="w-full h-auto flex flex-col items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full">
+        <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+          <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+            <p className="text-base text-red-500 font-semibold">
+              Bike Delievery
+            </p>
+            <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+              <img
+                src={Delivery}
+                alt="dle"
+                className="h-full w-full object-contain"
+              />
+            </div>
+          </div>
+          <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+            The Fastest Delievery In
+            <span className="text-red-500 text-[3rem] lg:text-[5rem]">
+              Your City
+            </span>
           </p>
-          <div className="w-6 h-6 bg-white rounded-full overflow-hidden drop-shadow-xl">
-            <img
-              src={Delivery}
-              className="w-full h-full object-contain"
-              alt="delievery"
-            />
+          <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and trinagele.
+          </p>
+          <a
+            href="#fruit"
+            type="button"
+            className="flex justify-center md:w-auto bg-gradient-to-br from-red-400 to-red-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          >
+            Order Now
+          </a>
+        </div>
+        <div className="py-2 flex-1 flex justify-end items-center relative">
+          <img
+            src={HeroBg}
+            className="absolute top-0 right-0 w-full h-420 md:w-auto md:h-650"
+          />
+          <div className="w-full md:w-460 ml-0 flex flex-wrap items-center justify-center gap-4 gap-y-14">
+            {showData &&
+              showData.map((data, i) => (
+                <motion.div
+                  key={i}
+                  {...steggerFadeInOut(i)}
+                  className="w-32 h-36 md:h-auto md:w-190 p-4 bg-lightOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+                >
+                  <img
+                    className="w-12 h-12 md:w-32 md:h-32  object-contain"
+                    src={data.imageSrc}
+                  />
+                  <p className="text-sm lg:text-xl font-semibold text-textColor capitalize">{data.name}</p>
+                </motion.div>
+              ))}
           </div>
         </div>
-        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-          The Fastest Delievery in{" "}
-          <span className="text-orange-700 text-[3rem] lg:text-[5rem] font-extrabold">
-            Your Town
-          </span>
-        </p>
-        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          trinagele.
-        </p>
-        <a
-          href="#fruit"
-          type="button"
-          className=" md:w-auto bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
-        >
-          Order Now
-        </a>
       </div>
-      {/* <div className="py-2  flex-1 flex items-center relative">
-        <img
-          className="h-420 full-width lg:w-auto lg:h-[650px] ml-auto"
-          src={HeroBg}
-          alt="hero"
-        />
-
-        <div className="h-full w-full absolute top-0 left-0 flex  flex-wrap justify-center items-center px-32 py-4 gap-2">
-          {showData &&
-            showData.map((data) => (
-              <div
-                key={data.id}
-                className="w-190 min-w-[190px] bg-cardOverlay backdrop-blur-md p-4 rounded-md flex flex-col items-center justify-center"
-              >
-                <img
-                  src={data.imageSrc}
-                  className="w-20 lg:w-40 -mt-10 lg:-mt-20"
-                  alt="I1"
-                />
-                <p className="text-textColor mt-2 lg:text-xl text-base font-semibold">
-                  {data.name}
-                </p>
-                <p className="text-[12px] lg:text-sm text-gray-700 my-1 lg:my-3">
-                  {data.des}
-                </p>
-                <p className="text-sm font-semibold text-headingColor">
-                  <span className="text-xs text-orange-600">$</span>
-                  {data.price}
-                </p>
-              </div>
-            ))}
-        </div>
-      </div> */}
     </section>
   );
 }
--- client/src/containers/main.jsx
+++ client/src/containers/main.jsx
@@ -15,9 +15,9 @@ export default function Main() {
     });
   }, []);
   return (
-    <main className="w-screen min-h-screen flex items-center  justify-center flex-col bg-primary">
+    <main className="w-full min-h-screen flex items-center  justify-center flex-col bg-primary">
       <Header />
-      <div className="w-full flex flex-col items-center justify-start mt-40 px-6 md:px-24 2xl:px-96 gap-12 pb-24 overflow-x-hidden">
+      <div className="w-full flex flex-col items-center justify-start mt-40 px-6 md:px-24 2xl:px-56 gap-12 pb-24 overflow-x-hidden">
         <Home />
         <HomeSlider />
         <FilterSection />
